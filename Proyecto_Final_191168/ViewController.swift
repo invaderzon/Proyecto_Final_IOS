f9edab2c388b641f0698cc312f5520ecbb236ec8
alumno <alumno@lab_quimica.uacj.mx>
2024-11-06 09:36:12 -0700

    Inicio del proyecto

---
+++ Proyecto_Final_191168/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Proyecto_Final_191168
+//
+//  Created by alumno on 11/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
